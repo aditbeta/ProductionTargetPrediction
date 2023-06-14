dd02c4ace898c4c382427cf955d3928aa42386db
aditbeta <aditbs00@gmail.com>
2023-06-15 05:58:05 +0700

    remove unused import

--- src/repository/ProductionRepository.java
+++ src/repository/ProductionRepository.java
@@ -8,9 +8,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import static service.helper.getMonths;
 
 public class ProductionRepository extends BaseRepository {
 
