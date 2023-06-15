b461152b7dcd25aec92c0a4d0635692c2fea44f0
aditbeta <aditbs00@gmail.com>
2023-06-16 06:07:25 +0700

    set default font

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import repository.ProductionRepository;
 import show.MainFrame;
 import show.ProductionInput;
 
+import javax.swing.*;
+import java.awt.*;
 import java.sql.*;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -14,6 +16,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        setUIFont (new javax.swing.plaf.FontUIResource("Segoe UI", Font.PLAIN,16));
+
         new MainFrame();
+    }
+
+    public static void setUIFont (javax.swing.plaf.FontUIResource f){
+        java.util.Enumeration keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get (key);
+            if (value instanceof javax.swing.plaf.FontUIResource)
+                UIManager.put (key, f);
+        }
     }
 }
